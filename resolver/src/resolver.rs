ed39905e9655ed12ba450c5586424f638e954c2b
Benjamin Fry <benjaminfry@me.com>
2017-05-23 09:36:35 -0700

    functional, very basic Resolver for TRust-DNS

--- resolver/src/resolver.rs
+++ resolver/src/resolver.rs
@@ -180,3 +180,42 @@ mod tests {
             .unwrap();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate tokio_core;
+
+    use std::net::{Ipv4Addr, ToSocketAddrs};
+    use self::tokio_core::reactor::Core;
+    use trust_dns::udp::UdpClientStream;
+
+    use super::*;
+
+    #[test]
+    fn test_lookup() {
+        let mut io_loop = Core::new().unwrap();
+        let addr: SocketAddr = ("8.8.8.8", 53)
+            .to_socket_addrs()
+            .unwrap()
+            .next()
+            .unwrap();
+        let (stream, sender) = UdpClientStream::new(addr, io_loop.handle());
+        let mut client = ClientFuture::new(stream, sender, io_loop.handle(), None);
+        let mut resolver =
+            Resolver::new(ResolverConfig::default(), ResolverOpts::default(), client);
+
+        io_loop
+            .run(resolver
+                     .lookup("www.example.com.")
+                     .map(move |response| {
+                              println!("response records: {:?}", response);
+
+                              let address = response[0];
+                              assert_eq!(address, IpAddr::V4(Ipv4Addr::new(93, 184, 216, 34)))
+                          })
+                     .map_err(|e| {
+                                  assert!(false, "query failed: {}", e);
+                              }))
+            .unwrap();
+    }
+}
